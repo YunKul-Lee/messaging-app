63be8815c3b293b6fa40441c2a7700ee59e8b110
jake <gerion@gmail.com>
2023-12-23 20:49:16 +0900

    Initial commit

---
+++ messaging-server/src/main/java/com/jake/messaging/server/MessagingServerApplication.java
@@ -0,0 +1,13 @@
+package com.jake.messaging.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingServerApplication.class, args);
+	}
+
+}
---
+++ messaging-server/src/test/java/com/jake/messaging/server/MessagingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.messaging.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
